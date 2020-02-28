ac223f376673781f3e5edc2e5be2c36d426bbb16
Matthew Ewan-Laws <matt.ewan-laws@hrgo.co.uk>
2020-02-28 11:07:51 +0000

    display header

---
+++ display.h
@@ -0,0 +1,10 @@
+#ifndef WINDOW_HEADER
+#define WINDOW_HEADER
+
+#include "window.h"
+
+#endif
+
+void add_strs(char *strs[], int num, struct Element *paneEl, int start_point,
+              int x, int y);
+
